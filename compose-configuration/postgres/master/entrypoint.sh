80550a23d4d3138764d24c83b0148a3a2d84acad
Arkadii Kudashov <8782808+ark85@users.noreply.github.com>
2026-08-18 02:34:38 +0500

    add master/replicas configuration

---
+++ compose-configuration/postgres/master/entrypoint.sh
@@ -0,0 +1,51 @@
+#!bin/bash
+
+set -e
+
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+    echo "Initializing PostgreSQL master..."
+    docker-entrypoint.sh postgres & POSTGRES_PID=$!
+    until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+        sleep 1
+    done
+
+    SUBNET=$(ip route | awk '
+        $1 != "default" && $3 == "" {
+            print $1
+            exit
+        }
+    ')
+
+    echo "Postgres subnet: $SUBNET"
+
+    PG_HBA="$PGDATA/pg_hba.conf"
+
+    REPLICATION_RULE="host replication replicator ${SUBNET} scram-sha-256"
+    if ! grep -Fxq "$REPLICATION_RULE" "$PG_HBA"; then
+        echo "$REPLICATION_RULE" >> "$PG_HBA"
+    fi
+
+    PGPASSWORD="${POSTGRES_PASSWORD}" psql \
+        -v ON_ERROR_STOP=1 \
+        -U "$POSTGRES_USER" \
+        -d "$POSTGRES_DB" \
+        -c "CREATE ROLE ${POSTGRES_REPLICATION_USER} WITH LOGIN REPLICATION PASSWORD '${POSTGRES_REPLICATION_PASSWORD}';"
+
+    # Reload pg config
+    PGPASSWORD="${POSTGRES_PASSWORD}" psql \
+        -v ON_ERROR_STOP=1 \
+        -U "$POSTGRES_USER" \
+        -d "$POSTGRES_DB" \
+        -c "SELECT pg_reload_conf();"
+
+    # Backup folder for replicas
+    mkdir -p /var/lib/postgresql/basebackup
+
+    PGPASSWORD="$POSTGRES_REPLICATION_PASSWORD" pg_basebackup \
+        -h pgmaster -D /var/lib/postgresql/basebackup -U ${POSTGRES_REPLICATION_USER} -v -P --wal-method=stream
+
+    kill "$POSTGRES_PID"
+    wait "$POSTGRES_PID" || true
+fi
+
+exec docker-entrypoint.sh postgres
