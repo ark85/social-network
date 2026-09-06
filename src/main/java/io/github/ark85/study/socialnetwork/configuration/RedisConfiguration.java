4fcf3f2b48addc519293411294aef0d66ece86bb
Arkadii Kudashov <8782808+ark85@users.noreply.github.com>
2026-09-06 23:55:28 +0500

    fix redis configuration

--- src/main/java/io/github/ark85/study/socialnetwork/configuration/RedisConfiguration.java
+++ src/main/java/io/github/ark85/study/socialnetwork/configuration/RedisConfiguration.java
@@ -4,30 +4,48 @@ import io.github.ark85.study.socialnetwork.model.Post;
 import io.github.ark85.study.socialnetwork.model.cache.PostEvent;
 import io.github.ark85.study.socialnetwork.service.cache.PostEventConsumer;
 import io.github.ark85.study.socialnetwork.utils.RedisStreamsConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisSystemException;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.stream.*;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.stream.StreamMessageListenerContainer;
 
 import java.time.Duration;
 
 @Configuration
+@Slf4j
 public class RedisConfiguration {
 
     @Bean
     public RedisTemplate<String, Post> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Post> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashKeySerializer(RedisSerializer.byteArray());
+        template.setHashValueSerializer(RedisSerializer.byteArray());
         return template;
     }
 
     @Bean
-    public StreamMessageListenerContainer<String, ObjectRecord<String, PostEvent>>
-    streamContainer(RedisConnectionFactory connectionFactory, PostEventConsumer postEventConsumer) {
+    public StreamMessageListenerContainer<String, ObjectRecord<String, PostEvent>> streamContainer(
+            RedisConnectionFactory connectionFactory,
+            PostEventConsumer postEventConsumer,
+            RedisTemplate<String, Post> redisTemplate) {
+        createConsumerGroup(redisTemplate);
         var options = StreamMessageListenerContainer.StreamMessageListenerContainerOptions
-                .builder().pollTimeout(Duration.ofSeconds(1)).targetType(PostEvent.class).build();
+                .builder()
+                .pollTimeout(Duration.ofSeconds(1))
+                .keySerializer(RedisSerializer.string())
+                .targetType(PostEvent.class)
+                .build();
         var container = StreamMessageListenerContainer.create(
                 connectionFactory,
                 options
@@ -46,5 +64,21 @@ public class RedisConfiguration {
         );
         container.start();
         return container;
+    }
+
+    private void createConsumerGroup(RedisTemplate<String, Post> redisTemplate) {
+        try {
+            redisTemplate.opsForStream().createGroup(
+                    RedisStreamsConstants.POST_EVENTS,
+                    ReadOffset.latest(),
+                    RedisStreamsConstants.CONSUMER_GROUP
+            );
+        } catch (RedisSystemException exception) {
+            if (exception.getMessage() != null && exception.getMessage().contains("BUSYGROUP")) {
+                log.info("Consumer group {} already exists.", RedisStreamsConstants.CONSUMER_GROUP);
+                return;
+            }
+            throw exception;
+        }
     }
 }
